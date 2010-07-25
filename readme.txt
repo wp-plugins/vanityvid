c1a182db47c54c198ff93d85c289a6fd2174fb90
orensol <orensol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-25 06:19:39 +0000

    Initial commit of 1.0.3 version
    git-svn-id: https://plugins.svn.wordpress.org/vanityvid/trunk@267167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Plugin Name ===
+Contributors: orensol
+Tags: vanityvid, video, avatar, profile pic
+Requires at least: 2.8.4
+Tested up to: 3.0
+Stable tag: 1.0.3
+
+Vanityvid turns your avatar into a video! 
+
+== Description ==
+
+Vanityvid turns avatars or profile pics into videos on your blog. Wherever your avatar shows, your Vanityvid will be attached to it.
+
+Having videos instead of just plain profile pics increases the interaction and engagement of your blog, makes it more social, and it's also just plain cool.
+
+== Installation ==
+
+1. Upload `vanityvid.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Open the Vanityvid admin page by going to Settings -> Vanityvid on your dashboard.
+1. Record your Vanityvid, confirm your email.
+1. Start seeing your Vanityvid on your blog wherever your avatar appears!
+
+== Frequently Asked Questions ==
+
+= How can I get a Vanityvid? =
+
+On the Vanityvid admin page, you can record directly from your webcam, upload a file, or send a video file by email.
+
+= Where will my Vanityvid appear? =
+
+Your Vanityvid will appear wherever your Wordpress avatar appears on your blog. It will also appear on other blogs that installed this plugin.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
+2. This is the second screen shot
+
+== Screenshots ==
+
+1. Vanityvid plugin admin screen. Here you can record your Vanityvid.
+2. Your avatar shows with a play button on it.
+3. When you click play, the video shows up!
+
+== Changelog ==
+
+= 1.0.3 =
+* Added screenshots.
+
+= 1.0 =
+* Initial official release.
+
---
+++ vanityvid.php
@@ -0,0 +1,159 @@
+<?php
+/*
+Plugin Name: Vanityvid
+Plugin URI: http://vanityvid.com/info/wordpress-plugin
+Description: Enable Vanityvid on your Wordpress blog! Turn your profile pic and your readers profile pics into Video!
+Version: 1.0.3
+Author: Vanityvid
+Author URI: http://vanityvid.com
+License: GPL2
+
+    Copyright 2010  Vanityvid
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+$vanityvidApiUrl = "http://vanityvid.com";
+
+/*
+ * 
+ * Main display functions
+ * 
+ */
+
+
+/**
+ * Writes js-resources script tag in <head>
+ *
+ * @return null
+ * 
+ */
+function vanityvid_head()
+{
+	global $vanityvidApiUrl;
+	
+	// add necessary javascript resources in html <HEAD>
+	?>
+	<script type="text/javascript">
+	vanityvid = {};
+	vanityvid.wordpress = true;
+	</script>
+	<script type="text/javascript" src="<?= $vanityvidApiUrl ?>/video/player/js-resources"></script>
+	<?php
+	
+}
+add_action('wp_head', 'vanityvid_head');
+
+/**
+ * Adds vanityvid="" attribute of email md5 to <img> tag, which upon page loads enables switching of pic to video
+ *
+ * Added as a filter to 'get_avatar'
+ * 
+ * @return string <img> tag with vanityvid attribute of email md5
+ */
+function vanityvid_show($avatar, $id_or_email, $size, $default, $alt)
+{
+	global $vanityvidApiUrl;
+	
+	// extract email
+	// adapted from plugabble.php
+	$email = '';
+	if ( is_numeric($id_or_email) ) {
+		$id = (int) $id_or_email;
+		$user = get_userdata($id);
+		if ( $user )
+			$email = $user->user_email;
+	} elseif ( is_object($id_or_email) ) {
+		if ( !empty($id_or_email->user_id) ) {
+			$id = (int) $id_or_email->user_id;
+			$user = get_userdata($id);
+			if ( $user)
+				$email = $user->user_email;
+		} elseif ( !empty($id_or_email->comment_author_email) ) {
+			$email = $id_or_email->comment_author_email;
+		}
+	} else {
+		$email = $id_or_email;
+	}
+
+	$vanityvid = str_replace("<img",'<img vanityvid="email:'.md5($email).'"',$avatar);
+	return $vanityvid;
+}
+add_filter('get_avatar', 'vanityvid_show', 10, 5);
+
+
+/*
+ * 
+ * Utility Functions
+ * 
+ */
+
+
+/*
+ * 
+ * Admin Configuration and Plugin Activation
+ * 
+ */
+
+function vanityvid_admin_menu()
+{
+	$plugin_page = add_options_page('Vanityvid', 'Vanityvid', 8, __FILE__, 'vanityvid_admin_page');
+	add_action( 'admin_head-'. $plugin_page, 'vanityvid_admin_head' );
+}
+
+function vanityvid_admin_head()
+{
+	global $vanityvidApiUrl, $current_user;
+	
+	// add necessary javascript resources in html <HEAD>
+	?>
+	<script type="text/javascript">
+	vanityvid = {};
+	vanityvid.enable_rec_button = true;
+	vanityvid.logged_in_user_id = '<?= md5($current_user->user_email) ?>';
+	vanityvid.logged_in_email = '<?= $current_user->user_email ?>';
+	</script>
+	<script type="text/javascript" src="<?= $vanityvidApiUrl ?>/video/player/js-resources"></script>
+	<?php
+}
+
+function vanityvid_admin_page()
+{
+	global $current_user;
+	?>
+	
+	<div class="wrap">
+		<h2>Your Vanityvid</h2>
+		<div style="border: 1px solid #262626; padding: 5px; float: left;">
+			<?= get_avatar($current_user->ID); ?>
+		</div>
+		<div style="clear:both;"></div>
+		<h2>Recording a Vanityvid</h2>
+		<div style="width:75%;">
+			If you haven't yet recorded a Vanityvid, you can press the 'REC' button on your profile pic above and record or upload a video here.
+			<br/><br/>
+			After you create a Vanityvid, you will receive an email confirmation to: <strong><?= $current_user->user_email ?></strong>.
+			<br/>Upon confirming successfully, your Vanityvid will be visible wherever your profile pic appears on your blog.
+			<br/><br/>
+			Enjoy!
+		</div>
+		<h2>Manage Vanityvids</h2>
+		<div style="width:75%;">To manage your Vanityvids, please visit the <a target="_blank" href="http://vanityvid.com">Vanityvid Website</a>.</div>
+	</div>
+
+	<?php
+}
+add_action('admin_menu','vanityvid_admin_menu');
+?>
